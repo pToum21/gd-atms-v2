c1ae49ef1768a9dfdcc9c1500d09035309ee9976
toum21 <pey2moo1@gmail.com>
2024-03-11 16:03:53 -0700

    added starter files to the server side code

---
+++ server/utils/auth.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+
+const { GraphQLError } = require('graphql');
+const jwt = require('jsonwebtoken');
+
+const secret = process.env.SECRET || 'ourlittlesecret';
+const expiration = '2h';
+
+module.exports = {
+    AuthenticationError: new GraphQLError('Could not authenticate user.', {
+        extensions: {
+            code: 'UNAUTHENTICATED',
+        },
+    }),
+    authMiddleware: function ({ req }) {
+        let token = req.body.token || req.query.token || req.headers.authorization;
+
+        if (req.headers.authorization) {
+            token = token.split(' ').pop().trim();
+        }
+
+        if (!token) {
+            return req;
+        }
+
+        try {
+            const { data } = jwt.verify(token, secret, { maxAge: expiration });
+            req.user = data;
+        } catch {
+            console.log('Invalid token');
+        }
+
+        return req;
+    },
+    signToken: function ({ email, username, _id, role }) {
+        const payload = { email, username, _id, role };
+        return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+    },
+};
