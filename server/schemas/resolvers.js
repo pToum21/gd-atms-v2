3163d8640879e00b9021aefef377169ec1f6657c
toum21 <pey2moo1@gmail.com>
2024-03-11 17:14:20 -0700

    added mutations for adding a user and logging in as a user

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,6 +4,35 @@ const { AuthenticationError, signToken } = require('../utils/auth');
 
 const resolvers = {
 
-};
+    Mutation: {
+        addUser: async (parent, { username, email, password }) => {
+            try {
+                const user = await User.create({ username, email, password });
+                const token = signToken(user);
+                return { token, user };
+            } catch (err) {
+                console.log(err);
+                throw new AuthenticationError('Something went wrong!');
+            }
+        },
 
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new AuthenticationError('No user found with this email address');
+            }
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        addReview: async () => {
+
+        }
+
+    }
+};
 module.exports = resolvers;
