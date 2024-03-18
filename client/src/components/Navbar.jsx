49c7b917f2c271aa653bb530c1e00bf22dafaac8
toum21 <pey2moo1@gmail.com>
2024-03-17 18:57:37 -0700

    fully rebuilt the navbar and changed font

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,16 +5,16 @@ import { Link } from 'react-router-dom';
 const NavBar = () => {
     return (
         <>
-            <AppBar position="fixed">
+            <AppBar position="fixed" elevation={0}>
                 <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', backgroundColor: 'white' }}>
-                    <Typography variant="h6" sx={{color: 'black'}}>My App</Typography>
+                    <Typography variant="h6" sx={{ color: 'black' }}>Logo</Typography>
                     <div style={{ display: 'flex', alignItems: 'center' }}>
-                        <div style={{ padding: '5%',border: '1px solid grey', borderRadius: '50px', display: 'flex', justifyContent: 'center', gap: '1rem' }}>
-                            <Link style={{textDecoration: 'none', fontSize: '1.2rem', color: 'black'}}>Reviews</Link>
-                            <Link style={{textDecoration: 'none', fontSize: '1.2rem', color: 'black'}}>Contact</Link>
+                        <div style={{ padding: '5%', border: '1px solid grey', borderRadius: '50px', display: 'flex', justifyContent: 'center', gap: '1rem' }}>
+                            <Link style={{ textDecoration: 'none', fontSize: '1.2rem', color: 'black' }}>Reviews</Link>
+                            <Link style={{ textDecoration: 'none', fontSize: '1.2rem', color: 'black' }}>Contact</Link>
                         </div>
                     </div>
-                    <Typography variant="h6" sx={{color: 'black'}}>My App</Typography>
+                    <Typography variant="h6" sx={{ color: 'black' }}>Login</Typography>
                 </Toolbar>
             </AppBar>
             <Toolbar />
