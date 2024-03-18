705dd541d18570f6893d8b93a71ee8ff17954c44
toum21 <pey2moo1@gmail.com>
2024-03-17 19:34:21 -0700

    updated nav style

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,16 +8,23 @@ const NavBar = () => {
         <>
             <AppBar position="fixed" elevation={0}>
                 <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', backgroundColor: 'white', height: '6.5vh' }}>
-                    <MuiLink variant="h6" component={Link} to="/" sx={{ color: 'black', textDecoration: 'none', fontSize: '1.8rem' }}>
-                        <span style={{ color: 'gray' }}>GD</span>ATMs
+                    <MuiLink variant="h6" component={Link} to="/" sx={{ color: 'black', textDecoration: 'none', fontSize: '1.8rem', }}>
+                        <span>
+                            <b>
+                                GD
+                            </b>
+                        </span>
+                        <span style={{ color: 'gray' }}>
+                            ATMs
+                        </span>
                     </MuiLink>
                     <div style={{ display: 'flex', alignItems: 'center', marginRight: '9%' }}>
                         <div style={{ padding: '5%', border: '1px solid grey', borderRadius: '50px', display: 'flex', justifyContent: 'center', gap: '1rem' }}>
-                            <MuiLink component={Link} to="/reviews" sx={{ textDecoration: 'none', fontSize: '1.2rem', color: 'black', '&:hover': { backgroundColor: 'rgba(128, 128, 128, 0.2)', borderRadius: '50px', padding: '2%' } }}>Reviews</MuiLink>
-                            <MuiLink component={Link} to="/contact" sx={{ textDecoration: 'none', fontSize: '1.2rem', color: 'black', '&:hover': { backgroundColor: 'rgba(128, 128, 128, 0.2)', borderRadius: '50px', padding: '2%' } }}>Contact</MuiLink>
+                            <MuiLink component={Link} to="/reviews" sx={{ textDecoration: 'none', fontSize: '1.1rem', color: 'black', '&:hover': { backgroundColor: 'rgba(128, 128, 128, 0.2)', borderRadius: '50px', padding: '2%' } }}>Reviews</MuiLink>
+                            <MuiLink component={Link} to="/contact" sx={{ textDecoration: 'none', fontSize: '1.1rem', color: 'black', '&:hover': { backgroundColor: 'rgba(128, 128, 128, 0.2)', borderRadius: '50px', padding: '2%' } }}>Contact</MuiLink>
                         </div>
                     </div>
-                    <MuiLink variant="h6" component={Link} to="/login" sx={{ color: 'black', textDecoration: 'none', '&:hover': { backgroundColor: 'rgba(128, 128, 128, 0.2)', borderRadius: '50px', border: '1px solid grey' } }}>Login</MuiLink>
+                    <MuiLink variant="h6" component={Link} to="/login" sx={{ color: 'black', textDecoration: 'none', }}>Login</MuiLink>
                 </Toolbar>
             </AppBar>
             <Toolbar />
