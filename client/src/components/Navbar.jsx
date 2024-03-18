9e93750d54ed4c47ad59dceeb243c1488b2c1524
toum21 <pey2moo1@gmail.com>
2024-03-17 18:31:52 -0700

    updated navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,23 @@
-function Navbar() {
-  return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
-  );
-}   
-export default Navbar;
+import React from 'react';
+import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+
+const NavBar = () => {
+    return (
+        <>
+            <AppBar position="fixed">
+                <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <Typography variant="h6">My App</Typography>
+                    <div sx={{ display: 'flex', gap: '1rem' }}>
+                        <Button color="inherit">Home</Button>
+                        <Button color="inherit">About</Button>
+                        <Button color="inherit">Services</Button>
+                        <Button color="inherit">Contact</Button>
+                    </div>
+                </Toolbar>
+            </AppBar>
+            <Toolbar />
+        </>
+    );
+};
+
+export default NavBar;
