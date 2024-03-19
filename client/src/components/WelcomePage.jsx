1cc210cccd5dc620324fb170a100eb2979d3700f
toum21 <pey2moo1@gmail.com>
2024-03-19 13:51:20 -0700

    chnaged font on the welcomepage

--- client/src/components/WelcomePage.jsx
+++ client/src/components/WelcomePage.jsx
@@ -4,9 +4,9 @@ import '../styles/welcome.css';
 const WelcomePage = () => {
   return (
     <div>
-      <p className="welcome-title" style={{ display: 'flex', justifyContent: 'center', marginBottom: '0.5rem' }}>Customer Centric Atm</p>
+      <h2 className="welcome-title" style={{ display: 'flex', justifyContent: 'center', marginBottom: '0.5rem' }}>Customer Centric Atm</h2>
       <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <p className="welcome-title" style={{ margin: '0' }}>Providers</p>
+        <h3 className="welcome-title" style={{ margin: '0' }}>Providers</h3>
       </div>
     </div>
   );
