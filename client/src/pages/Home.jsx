aae9fa89f68900217af3ba367b934e66325657dd
toum21 <pey2moo1@gmail.com>
2024-03-20 14:27:11 -0700

    began adding more to the welcome page component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,8 @@ import { Box, Typography } from '@mui/material';
 import WelcomePage from "../components/WelcomePage";
 import Three from "../components/Three/Three";
 
+import './home.css';
+
 function Home() {
   return (
     <div>
@@ -22,14 +24,14 @@ function Home() {
         <div
           style={{
             display: 'flex',
-            flexDirection: 'column',
+            flexDirection: 'row',
             width: '100%',
           }}
         >
-          <div>
+          <div style={{ width: '50%', padding: '2%' }}>
             <h1 style={{ color: 'white', fontSize: '3rem' }}>At GD ATMs we focus on getting you cash easier than ever.</h1>
           </div>
-          <div>
+          <div style={{ width: '50%', padding: '3%' }}>
             <p style={{ color: 'white', fontSize: '.9rem' }}>
               Your premier destination for convenient and affordable cash access solutions. With multiple locations strategically positioned across diverse neighborhoods and commercial hubs, we pride ourselves on being the go-to choice for anyone in need of quick and reliable cash transactions. At GD ATMs, we understand the importance of financial accessibility, which is why we've made it our mission to offer our services at the lowest possible cost to our valued customers. Our commitment to providing competitive transaction fees and ensuring operational excellence underscores our dedication to delivering a seamless and hassle-free experience. From bustling urban centers to suburban communities, our network of ATMs is designed to serve you wherever you are, empowering individuals with the financial flexibility they deserve. Discover the convenience and reliability of GD today, and experience firsthand how we're revolutionizing cash access for everyone.
             </p>
