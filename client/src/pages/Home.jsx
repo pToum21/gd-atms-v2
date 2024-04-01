a49ae5efe2143750cb515602643191d9fa3f5e0f
toum21 <pey2moo1@gmail.com>
2024-03-31 19:38:48 -0700

    added title text to the image portion of the webpage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -35,11 +35,11 @@ function Home() {
         </div>
       </Box>
       {/* this div holds the credit card image*/}
-      <div style={{ backgroundColor: 'black', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <div style={{ backgroundColor: 'black', display: 'flex', justifyContent: 'center', height: 'auto' }}>
         <div style={{ borderRadius: '50px', overflow: 'hidden', width: '100%', height: "80vh", padding: '1%', position: 'relative' }}>
           {/* Inserting the title div */}
           <div style={{ position: 'absolute', top: '7%', left: '23%', transform: 'translate(-50%, -50%)', textAlign: 'center', zIndex: '1', width: '40%' }}>
-            <h1 style={{ color: 'white', fontSize: '2.5rem', marginBottom: '10px', backgroundColor: '#eb7e95', borderRadius: '50px' }}>Accepted Cards</h1>
+            <h1 style={{ color: 'white', fontSize: '2rem', marginBottom: '10px', backgroundColor: '#eb7e95', borderRadius: '50px' }}>Accepted Cards</h1>
           </div>
           {/* End of title div */}
           <img src={creditCards} style={{ filter: 'grayscale(100%)', width: '100%', height: '100%', objectFit: 'cover', borderRadius: '50px' }} />
