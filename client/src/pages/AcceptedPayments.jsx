6178621070a8bb7c34509b8a673b4dba96e8c723
toum21 <pey2moo1@gmail.com>
2024-04-12 12:16:19 -0700

    added card icons

--- client/src/pages/AcceptedPayments.jsx
+++ client/src/pages/AcceptedPayments.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
-
+// all below are imports of credit card logos
+import twoCheckout from '../../public/images/card-icons/2checkout-curved-64px.png'
+import americanExpress from '../../public/images/card-icons/american-express-curved-64px.png'
+import cirrus from '../../public/images/card-icons/cirrus-curved-64px.png'
+import delta from '../../public/images/card-icons/delta-curved-64px.png'
+import directDebit from '../../public/images/card-icons/direct-debit-curved-64px.png'
+import discover from '../../public/images/card-icons/discover-curved-64px.png'
+import ebay from '../../public/images/card-icons/ebay-curved-64px.png'
+import googleCheckout from '../../public/images/card-icons/google-checkout-curved-64px.png'
+import maestro from '../../public/images/card-icons/maestro-curved-64px.png'
+import mastercard from '../../public/images/card-icons/mastercard-curved-64px.png'
+import moneybookers from '../../public/images/card-icons/moneybookers-curved-64px.png'
+import paypal from '../../public/images/card-icons/paypal-curved-64px.png'
+import sagePay from '/images/card-icons/sagepay-curved-64px.png'
+import solo from '/images/card-icons/solo-curved-64px.png'
+import visa from '/images/card-icons/visa-curved-64px.png'
+import visaElectron from '/images/card-icons/visa-electron-curved-64px.png'
+import westernUnion from '/images/card-icons/western-union-curved-64px.png'
 
 const AcceptedPayments = () => {
     // credit card icons
