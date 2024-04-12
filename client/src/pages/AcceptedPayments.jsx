6d0f754950bf5033f96f866ae5fb958922767400
toum21 <pey2moo1@gmail.com>
2024-04-12 12:18:54 -0700

    working on displaying the icons

--- client/src/pages/AcceptedPayments.jsx
+++ client/src/pages/AcceptedPayments.jsx
@@ -1,28 +1,47 @@
 import React from 'react';
-// all below are imports of credit card logos
-import twoCheckout from '/images/card-icons/2checkout-curved-64px.png'
-import americanExpress from '/images/card-icons/american-express-curved-64px.png'
-import cirrus from '/images/card-icons/cirrus-curved-64px.png'
-import delta from '/images/card-icons/delta-curved-64px.png'
-import directDebit from '/images/card-icons/direct-debit-curved-64px.png'
-import discover from '/images/card-icons/discover-curved-64px.png'
-import ebay from '/images/card-icons/ebay-curved-64px.png'
-import googleCheckout from '/images/card-icons/google-checkout-curved-64px.png'
-import maestro from '/images/card-icons/maestro-curved-64px.png'
-import mastercard from '/images/card-icons/mastercard-curved-64px.png'
-import moneybookers from '/images/card-icons/moneybookers-curved-64px.png'
-import paypal from '/images/card-icons/paypal-curved-64px.png'
-import sagePay from '/images/card-icons/sagepay-curved-64px.png'
-import solo from '/images/card-icons/solo-curved-64px.png'
-import visa from '/images/card-icons/visa-curved-64px.png'
-import visaElectron from '/images/card-icons/visa-electron-curved-64px.png'
-import westernUnion from '/images/card-icons/western-union-curved-64px.png'
+import Grid from '@material-ui/core/Grid';
+import twoCheckout from '/images/card-icons/2checkout-curved-64px.png';
+import americanExpress from '/images/card-icons/american-express-curved-64px.png';
+import cirrus from '/images/card-icons/cirrus-curved-64px.png';
+import delta from '/images/card-icons/delta-curved-64px.png';
+import directDebit from '/images/card-icons/direct-debit-curved-64px.png';
+import discover from '/images/card-icons/discover-curved-64px.png';
+import ebay from '/images/card-icons/ebay-curved-64px.png';
+import googleCheckout from '/images/card-icons/google-checkout-curved-64px.png';
+import maestro from '/images/card-icons/maestro-curved-64px.png';
+import mastercard from '/images/card-icons/mastercard-curved-64px.png';
+import moneybookers from '/images/card-icons/moneybookers-curved-64px.png';
+import paypal from '/images/card-icons/paypal-curved-64px.png';
+import sagePay from '/images/card-icons/sagepay-curved-64px.png';
+import solo from '/images/card-icons/solo-curved-64px.png';
+import visa from '/images/card-icons/visa-curved-64px.png';
+import visaElectron from '/images/card-icons/visa-electron-curved-64px.png';
+import westernUnion from '/images/card-icons/western-union-curved-64px.png';
 
 const AcceptedPayments = () => {
-    // credit card icons
+    // Array of credit card logo paths
+    const cardLogos = [
+        twoCheckout,
+        americanExpress,
+        cirrus,
+        delta,
+        directDebit,
+        discover,
+        ebay,
+        googleCheckout,
+        maestro,
+        mastercard,
+        moneybookers,
+        paypal,
+        sagePay,
+        solo,
+        visa,
+        visaElectron,
+        westernUnion
+    ];
 
     return (
-        <div style={{height: '90vh'}}>
+        <div>
             <h1>Accepted Payments</h1>
             
             <p>
@@ -31,6 +50,15 @@ const AcceptedPayments = () => {
             <p>
                 In addition to withdrawing cash, GD ATMs also provide the convenience of checking your bank balance directly from the ATM interface.
             </p>
+
+            {/* Grid container for credit card logos */}
+            <Grid container spacing={2} justify="center">
+                {cardLogos.map((logo, index) => (
+                    <Grid item xs={6} sm={3} md={2} key={index}>
+                        <img src={logo} alt={`Card Logo ${index}`} style={{ maxWidth: '100%', height: 'auto' }} />
+                    </Grid>
+                ))}
+            </Grid>
         </div>
     );
 };
