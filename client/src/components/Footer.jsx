5260188d72c48848b5866929fd945876efdcdf70
toum21 <pey2moo1@gmail.com>
2024-04-19 17:38:49 -0700

    updated footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     return (
         <footer style={{ backgroundColor: 'black' }}>
             <Box display="flex" justifyContent="space-between" maxWidth="800px" margin="0 auto " sx={{ backgroundColor: 'black' }}>
-                <Paper elevation={3} style={{ borderRadius: '15px', padding: '20px', backgroundColor: '#48494b', flex: 1, marginRight: '10px', marginTop: '10px', marginBottom: '20px'}}>
+                <Paper elevation={3} style={{ borderRadius: '15px', padding: '20px', backgroundColor: '#48494b', flex: 1, marginRight: '10px', marginTop: '10px', marginBottom: '20px' }}>
                     <Typography variant="body1" style={{ color: 'white' }}>
                         <strong>
                             <span>
@@ -22,12 +22,11 @@ const Footer = () => {
                             <li><Link component={RouterLink} to="/faq" color="inherit" underline="none" sx={{ '&:hover': { color: '#5D3FD3', textDecoration: 'underline', textDecorationColor: 'white' } }}>FAQ  ↗</Link></li>
                             <li><Link component={RouterLink} to="/about" color="inherit" underline="none" sx={{ '&:hover': { color: '#5D3FD3', textDecoration: 'underline', textDecorationColor: 'white' } }}>About  ↗</Link></li>
                             <li><Link component={RouterLink} to="/contact" color="inherit" underline="none" sx={{ '&:hover': { color: '#5D3FD3', textDecoration: 'underline', textDecorationColor: 'white' } }}>Contact  ↗</Link></li>
-                            <li><Link component={RouterLink} to="/our-csr" color="inherit" underline="none" sx={{ '&:hover': { color: '#5D3FD3', textDecoration: 'underline', textDecorationColor: 'white' } }}>Our CSR  ↗</Link></li>
                             <li><Link component={RouterLink} to="/privacy-policy" color="inherit" underline="none" sx={{ '&:hover': { color: '#5D3FD3', textDecoration: 'underline', textDecorationColor: 'white' } }}>Privacy Policy  ↗</Link></li>
                         </ul>
                     </Typography>
                 </Paper>
-                <Paper elevation={3} style={{ borderRadius: '15px', padding: '20px', backgroundColor: '#48494b', flex: 1, marginLeft: '10px', marginTop: '10px', marginBottom: '20px'}}>
+                <Paper elevation={3} style={{ borderRadius: '15px', padding: '20px', backgroundColor: '#48494b', flex: 1, marginLeft: '10px', marginTop: '10px', marginBottom: '20px' }}>
                     <Typography variant="body1" style={{ color: 'white' }}>
                         <strong>Cities</strong>
                         <ul style={{ listStyle: 'none', paddingLeft: 0 }}>
