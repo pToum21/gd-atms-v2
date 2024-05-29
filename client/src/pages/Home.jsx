e83469a352d73ebf65b123952af83b2bac9f3879
toum21 <pey2moo1@gmail.com>
2024-05-28 18:58:43 -0700

    adding more to the home screen

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { Box } from '@mui/material';
 import { Link } from 'react-router-dom';
 import WelcomePage from "../components/WelcomePage";
 import Three from "../components/Three/Three";
+import TerminalNews from "../components/TerminalNews";
 
 import creditCards from '/images/creditcards.jpg';
 
@@ -99,6 +100,36 @@ function Home() {
           )}
         </div>
       </div>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          backgroundColor: 'black',
+          borderRadius: '0px 0px 0px 0px',
+          padding: '20px',
+          height: 'auto',
+          '@media screen and (min-width: 768px)': {
+            flexDirection: 'row',
+          },
+        }}
+      >
+        <Box
+          sx={{
+            backgroundColor: 'grey',
+            borderRadius: '10px',
+            padding: '20px',
+            margin: '10px',
+            flex: '1',
+          }}
+        >
+          <h1 style={{ color: 'white', fontSize: '2.6rem', marginBottom: '10px' }}>
+            At GD ATMs we focus on getting you cash easier than ever.
+          </h1>
+          <p style={{ color: 'white', fontSize: '.9rem', marginBottom: '10px', maxWidth: '100%' }}>
+            Your premier destination for convenient and affordable cash access solutions. With multiple locations strategically positioned across diverse neighborhoods and commercial hubs, we pride ourselves on being the go-to choice for anyone in need of quick and reliable cash transactions. At GD ATMs, we understand the importance of financial accessibility, which is why we've made it our mission to offer our services at the lowest possible cost to our valued customers. Our commitment to providing competitive transaction fees and ensuring operational excellence underscores our dedication to delivering a seamless and hassle-free experience. From bustling urban centers to suburban communities, our network of ATMs is designed to serve you wherever you are, empowering individuals with the financial flexibility they deserve. Discover the convenience and reliability of GD today, and experience firsthand how we're revolutionizing cash access for everyone.
+          </p>
+        </Box>
+      </Box>
     </div>
   );
 }
