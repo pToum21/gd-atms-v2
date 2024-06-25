3610b4988cf7ca03ba276640406f02808380e036
toum21 <pey2moo1@gmail.com>
2024-06-24 19:25:13 -0700

    removed an unnessacary import in Three.jsx

--- client/src/components/Three/Three.jsx
+++ client/src/components/Three/Three.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
 import Experience from './Experience';
 
 const Three = () => {
